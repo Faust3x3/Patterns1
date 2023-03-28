eb0de025e993df6e928649c0f98c58ef65ae88a0
Arrow234 <forgit@faust3x3.com>
2023-03-28 16:42:56 +0300

    first

---
+++ src/test/java/test/data/User.java
@@ -0,0 +1,14 @@
+package test.data;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+
+public class User {
+    private final String city;
+    private final String data;
+    private final String name;
+    private final String phone;
+}
